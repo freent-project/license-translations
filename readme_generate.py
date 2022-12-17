15601b063d4760c5006704f2566f5358d8ae82dd
Tyler Magee Shields <115214762+TylerMS887@users.noreply.github.com>
2022-12-17 14:53:08 +0000

    Update readme_generate.py

--- readme_generate.py
+++ readme_generate.py
@@ -6,7 +6,7 @@ f = open("readme.md", "w")
 for file in glob.glob("*/*.license"):
   print(file + ":")
   print(open(file, "r").read())
-  readme = readme + "\n## " + file = "\n```" + open(file, "r").read() = "\n```\n\n"
+  readme = readme + "\n## " + file + "\n```" + open(file, "r").read() + "\n```\n\n"
 print("This is what the file will look like AFTER the readme is generated:")
 print(readme)
 print("Will write README in 3 seconds.")
