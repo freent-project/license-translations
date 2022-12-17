0ce49d1a909c620c1c64bf52fc33d7c4a0830757
Tyler Magee Shields <115214762+TylerMS887@users.noreply.github.com>
2022-12-17 14:45:15 +0000

    Create readme_generate.py

---
+++ readme_generate.py
@@ -0,0 +1,18 @@
+import os
+import glob
+import time
+readme = "# FreeNT OpenWindows License\n"
+f = open("readme.md", "w")
+for file in glob.glob("*/*.license"):
+  print(file + ":")
+  print(open(file, "r").read())
+  readme = readme = "\n## " + file = "\n```" + open(file, "r").read() = "\n```\n\n"
+print("This is what the file will look like AFTER the readme is generated:")
+print(readme)
+print("Will write README in 3 seconds.")
+time.sleep(3)
+print("Writing...")
+f.write(readme)
+print("Closing file...")
+f.close()
+print("Generated and written README!")
