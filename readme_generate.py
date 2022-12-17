e7ba3e01e142091506993f424f541705d5821332
Tyler Magee Shields <115214762+TylerMS887@users.noreply.github.com>
2022-12-17 15:45:56 +0000

    Update readme_generate.py

--- readme_generate.py
+++ readme_generate.py
@@ -1,14 +1,15 @@
 import os
 import glob
 import time
-readme = "# FreeNT OpenWindows License\n\nThese are official, localized versions of the OpenWindows license created by the OpenWindows project.\n\n## Languages\n\n"
+readme = "# FreeNT OpenWindows License\n\nThese are official, localized versions of the OpenWindows license created by the OpenWindows project.\n\n## List of Languages\n\n"
 f = open("readme.md", "w")
 for file in glob.glob("*/*.license"):
   readme = readme + "- [" + os.path.basename(file).split(".")[0] + "](#" + os.path.basename(file).split(".")[0].replace(" ", "-").lower() + ")\n"
+readme = readme + "## Languages"
 for file in glob.glob("*/*.license"):
   print(file + ":")
   print(open(file, "r").read())
-  readme = readme + "\n## " + os.path.basename(file).split(".")[0] + "\n```\n" + open(file, "r").read() + "\n```\n\n"
+  readme = readme + "\n### " + os.path.basename(file).split(".")[0] + "\n```\n" + open(file, "r").read() + "\n```\n\n"
 print("This is what the file will look like AFTER the readme is generated:")
 print(readme)
 print("Will write README in 3 seconds.")
