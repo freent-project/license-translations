0b7e01a2f33c7f496660b8fa3cd95bbf7b3bbec2
Tyler Magee Shields <115214762+TylerMS887@users.noreply.github.com>
2022-12-18 17:09:22 +0000

    Update readme_generate.py

--- readme_generate.py
+++ readme_generate.py
@@ -10,7 +10,7 @@ readme = readme + "## Languages"
 for file in sorted(glob.glob("*/*.license")):
   print(file + ":")
   print(open(file, "r").read())
-  readme = readme + "\n### " + os.path.basename(file).split(".")[0] + "\n```\n" + open(file, "r").read() + "```\nYou can get a copy of this license as text [here](/" + file + ") or ~~Braille~~ (*not yet, but soon*)."
+  readme = readme + "\n### " + os.path.basename(file).split(".")[0] + "\n```\n" + open(file, "r").read() + "```\nYou can get a copy of this license as [plain text](/" + file + ") or ~~Braille~~ (*not yet, but soon*)."
 print("This is what the file will look like AFTER the readme is generated:")
 print(readme)
 print("Will write README in 3 seconds.")
