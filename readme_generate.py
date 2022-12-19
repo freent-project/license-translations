bf35e5449845841a26aecc378613301e7e5b2d8d
Tyler Magee Shields <115214762+TylerMS887@users.noreply.github.com>
2022-12-19 21:49:04 +0000

    Add URL encoding to links
    The link markup is broken without encoding, so we encode the path in URL style

--- readme_generate.py
+++ readme_generate.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import time
+from urllib.parse import quote
 readme = "# FreeNT OpenWindows License\n\nThese are official, localized versions of the OpenWindows license created by the OpenWindows project.\n\n## List of Languages\n\n"
 f = open("readme.md", "w")
 for file in sorted(glob.glob("*/*.license")):
@@ -10,7 +11,7 @@ readme = readme + "## Languages"
 for file in sorted(glob.glob("*/*.license")):
   print(file + ":")
   print(open(file, "r").read())
-  readme = readme + "\n### " + os.path.basename(file).split(".")[0] + "\n```\n" + open(file, "r").read() + "```\nAlso available as:\n- [Plain text](/" + file + ")\n- ~~Braille~~ (not yet, but soon)"
+  readme = readme + "\n### " + os.path.basename(file).split(".")[0] + "\n```\n" + open(file, "r").read() + "```\nAlso available as:\n- [Plain text](/" + quote(file) + ")\n- ~~Braille~~ (not yet, but soon)"
 print("This is what the file will look like AFTER the readme is generated:")
 print(readme)
 print("Will write README in 3 seconds.")
